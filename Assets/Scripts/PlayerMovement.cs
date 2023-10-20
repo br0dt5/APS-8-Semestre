61a274816213b6f85324a3efd78350c31e55b3a4
Davi de Godoy Ferreira <davigf2002@gmail.com>
2023-10-19 22:45:00 -0300

    style(assets): Adicionado estrutura de pastas e criando movimento base

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed;
+
+    private void Update()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+
+        Vector3 direction = new Vector3(horizontal, vertical);
+        transform.position = direction * speed * Time.deltaTime;
+    }
+}
